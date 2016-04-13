b01a27a8e22c95929d99061fcbea78d3e8348e63
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-04-13 21:45:26 +0200

    Implement suggestions from external search

--- src/marc.js
+++ src/marc.js
@@ -10,7 +10,8 @@ function subfieldVal(marcField, subfield) {
 
 
 export function marc2rdf(marc, subject, ns) {
-	let g = new Graph
+	let graph = new Graph
+	let suggestions = []
 
 	// TODO move out
 	let pred = function(pred) { return new NamedNode(ns+pred) }
@@ -21,29 +22,48 @@ export function marc2rdf(marc, subject, ns) {
 
 	for (let f of marc.datafield) {
 		switch (f.$.tag) {
+			case '100':
+				let creator = {
+					name: subfieldVal(f, "a")
+				}
+				let lifespan  = subfieldVal(f, "d")
+				if (lifespan) {
+					creator.lifespan = lifespan
+				}
+				let nationality = subfieldVal(f, "j")
+				if (nationality) {
+					creator.nationality = nationality
+				}
+				suggestions.push({
+					type: "MainEntry",
+					label: creator.name,
+					source: "?",
+					value: creator
+				})
+				break
 			case '245':
 				let mainTitle = subfieldVal(f, "a")
 				if (mainTitle) {
-					g.insert(new Triple(subj, pred('mainTitle'), lit(mainTitle)))
+					graph.insert(new Triple(subj, pred('mainTitle'), lit(mainTitle)))
 				}
 				let subtitle = subfieldVal(f, "b")
 				if (subtitle) {
-					g.insert(new Triple(subj, pred('subtitle'), lit(subtitle)))
+					graph.insert(new Triple(subj, pred('subtitle'), lit(subtitle)))
 				}
 				break
 			case '260':
 				let publishYear = subfieldVal(f, "c")
 				if (publishYear) {
-					g.insert(new Triple(subj, pred('publishYear'), lit(publishYear)))
+					graph.insert(new Triple(subj, pred('publishYear'), lit(publishYear)))
 				}
 				break
 			case '300':
 				let numPages = subfieldVal(f, "a")
 				if (numPages) {
-					g.insert(new Triple(subj, pred('numPages'), lit(numPages.replace(/\D/g,''))))
+					graph.insert(new Triple(subj, pred('numPages'), lit(numPages.replace(/\D/g,''))))
 				}
 				break
 		}
 	}
-	return g
+	return {graph, suggestions}
 }
--- src/state.js
+++ src/state.js
@@ -1,5 +1,5 @@
 import { DB } from './db.js'
-
+import riot from 'riot'
 // The resource currently beeing edited; the "center" node of the graph.
 let centerNode = ""
 
@@ -9,9 +9,12 @@ let db = new DB
 // Ontology namespace
 let ns = "http://192.168.50.12:8005/ontology#"
 
+let suggestions = riot.observable()
+
 // The state object with global variables:
 export default {
 	db,
 	centerNode,
-	ns
+	ns,
+	suggestions
 }
